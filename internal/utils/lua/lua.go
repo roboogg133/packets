76450789b0548420405287a83dd4e728dc497aa7
roboogg133 <Luizfernandojoaquimdasilva@proton.me>
2025-09-28 21:55:50 -0300

    io can be used, require too, and package

--- internal/utils/lua/lua.go
+++ internal/utils/lua/lua.go
@@ -16,10 +16,7 @@ func GetSandBox(sandboxdir string) (lua.LState, error) {
 	}
 	L := lua.NewState()
 	osObject := L.GetGlobal("os").(*lua.LTable)
-	ioObject := L.GetGlobal("io").(*lua.LTable)
 
-	L.SetGlobal("package", lua.LNil)
-	L.SetGlobal("require", lua.LNil)
 	L.SetGlobal("SAFE_MODE", lua.LTrue)
 
 	L.SetGlobal("PACKETS_DATADIR", lua.LString(cfg.Config.Data_d))
@@ -35,14 +32,6 @@ func GetSandBox(sandboxdir string) (lua.LState, error) {
 	osObject.RawSetString("symlink", L.NewFunction(LSymlink))
 	osObject.RawSetString("mkdir", L.NewFunction(LMkdir))
 
-	ioObject.RawSetString("input", lua.LNil)
-	ioObject.RawSetString("output", lua.LNil)
-	ioObject.RawSetString("popen", lua.LNil)
-	ioObject.RawSetString("tmpfile", lua.LNil)
-	ioObject.RawSetString("stdout", lua.LNil)
-	ioObject.RawSetString("stderr", lua.LNil)
-	ioObject.RawSetString("stdin", lua.LNil)
-	ioObject.RawSetString("lines", lua.LNil)
 	//ioObject.RawSetString("open", L.NewFunction(LOpen))
 
 	return *L, nil
