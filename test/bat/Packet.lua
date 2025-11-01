4c88ec3bc2d2f5fef426a216e96e8667e65b2361
robogg133 <Luizfernandojoaquimdasilva@proton.me>
2025-10-31 21:32:51 -0300

    using CURRENT_ARCH_NORMALIZED

--- test/bat/Packet.lua
+++ test/bat/Packet.lua
@@ -1,11 +1,3 @@
-local arch_map = {
-    amd64 = "x86_64",
-    aarch64 = "aarch64",
-    arm64 = "aarch64",
-    ['386'] = "i686" 
-}
-local srcarch = arch_map[CURRENT_ARCH]
-
 return {
     package = {
         name = "bat-bin", -- required
@@ -19,7 +11,7 @@ return {
                 arch = {"amd64"},
                 sources = {
                     {
-                        url = "https://github.com/sharkdp/bat/releases/download/v0.26.0/bat-v0.26.0-" ..srcarch .."-pc-windows-msvc.zip",
+                        url = "https://github.com/sharkdp/bat/releases/download/v0.26.0/bat-v0.26.0-" ..CURRENT_ARCH_NORMALIZED.."-pc-windows-msvc.zip",
                         method = "GET",
                         sha256="a8a6862f14698b45e101b0932c69bc47a007f4c0456f3a129fdcef54d443d501"
                     }
@@ -34,7 +26,7 @@ return {
                 arch = {"amd64"},
                 sources = {
                     {
-                        url = "https://github.com/sharkdp/bat/releases/download/v0.26.0/bat-v0.26.0-".. srcarch .."-unknown-linux-gnu.tar.gz",
+                        url = "https://github.com/sharkdp/bat/releases/download/v0.26.0/bat-v0.26.0-".. CURRENT_ARCH_NORMALIZED .."-unknown-linux-gnu.tar.gz",
                         method = "GET",
                         sha256 = "7efed0c768fae36f18ddbbb4a38f5c4b64db7c55a170dfc89fd380805809a44b"
                     }
